b844178e8528dea96fbfbfd7c551d42f4c3a8de0
Abdillahi Aden <abdillahi1516@gmail.com>
2026-05-22 08:09:35 +0200

    Ajout sslmode pour Supabase

--- test_streamlit_final_v2.py
+++ test_streamlit_final_v2.py
@@ -25,7 +25,7 @@ def connect_db():
         user=os.getenv("DB_USER"),
         password=os.getenv("DB_PASSWORD"),
         host=os.getenv("DB_HOST"),
-        port=os.getenv("DB_PORT")
+        port=os.getenv("DB_PORT"),
         sslmode="require"
     )
 
